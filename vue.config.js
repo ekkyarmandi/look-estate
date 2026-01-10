e1633832c06e4ed860b3b522f1a9341d08026fc5
ekkyarmandi <me@ekky.dev>
2026-01-11 01:29:14 +0700

    fix: bring back dev server proxy for develop env

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,18 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  devServer: {
+    proxy: {
+      "^/properties": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        pathRewrite: { "^/properties": "/properties" },
+      },
+      "^/property": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        pathRewrite: { "^/property": "/property" },
+      },
+    },
+  },
 });
